4cd90c22a6c26979d462d1a28f8eff3e72e77c64
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-02-12 17:42:52 +0200

    add auth form

--- src/components/layout/header/icons-right/index.tsx
+++ src/components/layout/header/icons-right/index.tsx
@@ -1,12 +1,13 @@
 import React, {FC} from 'react';
 
+import AuthForm from '@components/layout/header/auth-form';
 import ProfileMenu from '@components/layout/header/profile-menu';
 
 const IconsRight: FC = () => {
-  const isAuth: boolean = true;
+  const isAuth: boolean = false;
 
   if (!isAuth) {
-    return null;
+    return <AuthForm />;
   }
 
   return <ProfileMenu />;
--- src/components/layout/header/profile-menu/index.tsx
+++ src/components/layout/header/profile-menu/index.tsx
@@ -3,11 +3,13 @@ import React, {FC} from 'react';
 import avatarImage from '@assets/images/avatar.jpg';
 import {ReactComponent as DownArrow} from '@assets/images/down-arrow.svg';
 
+import {useAuth} from '@/hooks/useAuth';
+
 import classes from './ProfileMenu.module.scss';
 
 const ProfileMenu: FC = () => {
-  const avatar: string = avatarImage;
-  const username: string = 'kasim876';
+  const {user} = useAuth();
+  const avatar = avatarImage;
 
   return (
     <button className={classes.root}>
@@ -16,7 +18,7 @@ const ProfileMenu: FC = () => {
         src={avatar}
         alt=""
       />
-      <span className={classes.username}>{username}</span>
+      <span className={classes.username}>{user?.username}</span>
       <DownArrow className={classes.icon} />
     </button>
   );
