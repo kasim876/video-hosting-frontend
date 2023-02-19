8b1cfa4605e795296c7b59e6ee0fa732f113558a
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-02-19 23:05:28 +0200

    add solid button

---
+++ src/ui/ButtonSolid/index.tsx
@@ -0,0 +1,19 @@
+import cn from 'clsx';
+import {ButtonHTMLAttributes, PropsWithChildren, forwardRef} from 'react';
+
+import classes from './ButtonSolid.module.scss';
+
+export const ButtonSolid = forwardRef<
+  HTMLButtonElement,
+  PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>
+>(({children, className, ...props}, ref) => {
+  return (
+    <button
+      className={cn(classes.root, className)}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+});
