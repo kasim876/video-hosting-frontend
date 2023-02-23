30a14bcf04e2964d056e318fda4b6b969ec68d56
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-02-23 21:07:45 +0200

    add hook

---
+++ src/hooks/useOutside.ts
@@ -0,0 +1,28 @@
+import {Dispatch, SetStateAction, useEffect, useRef, useState} from 'react';
+
+type TypeOut = {
+  ref: any;
+  isShow: boolean;
+  setIsShow: Dispatch<SetStateAction<boolean>>;
+};
+
+export default function useOutside(initialIsVisible: boolean): TypeOut {
+  const [isShow, setIsShow] = useState(initialIsVisible);
+  const ref = useRef<HTMLElement>(null);
+
+  const handleClickOutside = (event: any) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsShow(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  });
+
+  return {ref, isShow, setIsShow};
+}
