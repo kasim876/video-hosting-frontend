26e7bf6e530d000da7c564bea4d48fcbdf2ce923
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-11 17:04:17 +0300

    add video reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 
 import userReducer from './slices/userSlice';
+import videoReducer from './slices/videoSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    video: videoReducer,
   },
 });
 
