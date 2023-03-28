84244032673829ccc298d78da6bbb1b05eaeb3c9
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-28 13:31:50 +0300

    update user interface

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -5,6 +5,7 @@ import {IVideo} from '@/types/video.interface';
 export interface IUser extends IBase {
   email: string;
   name: string;
+  description: string;
   subscribersCount?: number;
   avatarPath: string;
   videos: IVideo[];
