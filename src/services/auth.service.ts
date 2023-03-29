8fc8214ac3068abdf30f9b05515c0c333199f5ea
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-29 09:43:43 +0300

    add auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,26 @@
+import {$host} from '@/api/axios';
+
+import {IAuthData} from '@/types/auth.interface';
+
+const AuthService = {
+  async register(email: string, name: string, password: string) {
+    const response = await $host.post<IAuthData>('/auth/register', {
+      email,
+      name,
+      password,
+    });
+
+    return response.data;
+  },
+
+  async login(email: string, password: string) {
+    const response = await $host.post<IAuthData>('/auth/login', {
+      email,
+      password,
+    });
+
+    return response.data;
+  },
+};
+
+export default AuthService;
