b6190cae426c1d53e911b83e80afc7e8fc018c0e
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-30 11:07:44 +0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  poweredByHeader: false,
+  env: {
+    REACT_APP_API: process.env.REACT_APP_API,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
