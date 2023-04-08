1085720ae5453d6c3e1b4198bf0ef147af432503
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-04-08 16:02:04 +0300

    add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type {AppProps} from 'next/app';
 import NextProgressBar from 'nextjs-progressbar';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 
@@ -7,19 +8,23 @@ import store, {persistor} from '@/store/store';
 
 import '@/styles/globals.scss';
 
+const queryClient = new QueryClient();
+
 export default function App({Component, pageProps}: AppProps) {
   return (
-    <Provider store={store}>
-      <PersistGate
-        persistor={persistor}
-        loading={null}
-      >
-        <NextProgressBar
-          height={2}
-          color="#ff7652"
-        />
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate
+          persistor={persistor}
+          loading={null}
+        >
+          <NextProgressBar
+            height={2}
+            color="#ff7652"
+          />
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </QueryClientProvider>
   );
 }
